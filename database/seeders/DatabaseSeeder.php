5c4cb952ff80088764fee840120c96eabf7d96c5
Devv <bendanodevv@gmail.com>
2025-07-06 11:48:14 +0800

    Initial 2: added api & username

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Admin',
+            'username' => 'admin',
+            'email' => 'test@admin.com',
+            'password' => 'admin123'
         ]);
     }
 }
